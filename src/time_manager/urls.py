c79a8baa1bad91c082c58a0398f24bdc0e7fb147
Nikita Vakula <programmistov.programmist@gmail.com>
2020-05-16 22:50:53 +0200

    Added admindocs app
    This way it is easy to see the docs, etc.
    
    Signed-off-by: Nikita Vakula <programmistov.programmist@gmail.com>

--- src/time_manager/urls.py
+++ src/time_manager/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
 ]
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
