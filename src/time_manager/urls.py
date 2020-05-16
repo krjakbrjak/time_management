81f0939d14f517d2783442336f693e762e88c90a
Nikita Vakula <programmistov.programmist@gmail.com>
2020-05-16 19:30:38 +0200

    Serve static files in DEBUG mode
    When gunicorn is used (or better to say not django's server)
    then STATIC_ROOT has to be set.
    IMPORTANT: run `python migrate.py collectstatic` beforehand
    
    Signed-off-by: Nikita Vakula <programmistov.programmist@gmail.com>

--- src/time_manager/urls.py
+++ src/time_manager/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
