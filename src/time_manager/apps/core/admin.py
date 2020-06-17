83f9a578e1d32790bee14ea90539991f841cb3f8
Nikita Vakula <programmistov.programmist@gmail.com>
2020-06-17 22:16:51 +0200

    core: updated admin site
    Signed-off-by: Nikita Vakula <programmistov.programmist@gmail.com>

--- src/time_manager/apps/core/admin.py
+++ src/time_manager/apps/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Request, RequestType, Date, Comment
+from .models import TimeRequest, RequestType, Comment
 from django.utils.translation import ngettext
 from django.contrib import messages
 from django.urls import reverse
@@ -12,66 +12,9 @@ from datetime import datetime
 class CommentAdmin(admin.ModelAdmin):
     search_fields = ('comment',)
 
-def convert_int_to_format(value, format):
-    """ Converts an integer to a date string
-    with the format.
-
-    Parameters:
-    value (int): value to convert
-    format (str): time format
-
-    Returns:
-    string representation of a timestamp or an original value
-    """
-
-    if isinstance(value, int):
-        return datetime.fromtimestamp(value).strftime(format)
-
-    return value
-
-class TimeFromIntegerInput(forms.widgets.TimeInput):
-    """ Time input widget with the backing integer field,
-    that represents the number of epoch timestamp (seconds).
-    """
-
-    def format_value(self, value):
-        return super().format_value(convert_int_to_format(value, self.format))
-
-    def get_context(self, name, value, attrs):
-        return super().get_context(name, convert_int_to_format(value, self.format), attrs)
-
-class TimeField(forms.IntegerField):
-    format = '%H:%M'
-    widget = TimeFromIntegerInput(format=format, attrs={'type': 'time'})
-    def to_python(self, value):
-        """Normalize data to an integer."""
-        if not value:
-            return 0
-
-        pt = datetime.strptime(value, self.format)
-
-        return pt.minute*60 + pt.hour*3600
-
-    def validate(self, value):
-        """Check if value consists only of valid emails."""
-        super().validate(value)
-
-class RequestForm(forms.ModelForm):
-    end = TimeField(required=True)
-    start = TimeField(required=True)
-
-    class Meta:
-        model = Request
-        fields = '__all__'
-
-@admin.register(Request)
+@admin.register(TimeRequest)
 class ShowDateAdmin(admin.ModelAdmin):
-    form = RequestForm
-    list_filter = ('user',)
-
-@admin.register(Date)
-class DateAdmin(admin.ModelAdmin):
-    list_display = ('ts',)
+    list_filter = ('user', 'start')
 
 class ReadOnlyMixin():
 
